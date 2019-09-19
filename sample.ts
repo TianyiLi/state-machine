14e079a327572b56571a1334dfc4c536ce62c9d7
Paul Lee <e0991100238@gmail.com>
2019-09-19 17:38:35 +0800

    Updates

---
+++ sample.ts
@@ -0,0 +1,55 @@
+import { StateMachine } from '../src/main'
+
+let smc = new StateMachine({
+  onTransition: {
+    '*': console.dir,
+    'END': async ({ arg }) => {
+      if (arg && arg[0] && arg[0].sec) {
+        await new Promise((resolve, rej) => {
+          setTimeout(() => {
+            resolve()
+          }, arg[0].sec * 1000)
+        })
+      }
+    }
+  },
+  initState: 'none',
+  transitions: [
+    {
+      action: 'goto',
+      from: '*',
+      to: function(s) {
+        return s
+      }
+    },
+    {
+      from: 'none',
+      action: 'start',
+      to: 'START',
+      guardian(argv: any = {}) {
+        if (argv.test) return true
+        else return false
+      }
+    },
+    { from: 'START', action: 'read', to: 'START' },
+    { from: 'START', action: 'stop', to: 'END' },
+    { from: 'END', action: 'reset', to: 'none' }
+  ]
+})
+async function test() {
+  smc.on('START', console.log)
+  smc.once('END', console.log)
+  smc.step('start')
+  console.log(smc.getState())
+  smc.step('start', { test: true })
+  console.log(smc.getState())
+  console.log(smc.getStateList())
+  console.log(smc.getMethods('START'))
+  smc.step('goto', 'none')
+  console.log(smc.getState())
+  smc.getStateList()
+  smc.step('start', { test: 'test' }, 1, 2, 3)
+  await smc.step('stop', { sec: 10 })
+  smc.step('goto', 'none')
+}
+test()
